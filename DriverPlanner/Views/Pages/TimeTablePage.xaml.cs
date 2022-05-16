78dc54e4bb29be1e1c00af738209e6653264a083
Kate <2003katy2003@mail.ru>
2022-05-16 17:19:45 +0300

    .

---
+++ DriverPlanner/Views/Pages/TimeTablePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DriverPlanner.Views.Pages
+{
+	/// <summary>
+	/// Логика взаимодействия для TimeTablePage.xaml
+	/// </summary>
+	public partial class TimeTablePage : UserControl
+	{
+		public TimeTablePage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ DriverPlanner/Views/Pages/WorkingPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace DriverPlanner.Views.Pages
+{
+	/// <summary>
+	/// Логика взаимодействия для WorkingPage.xaml
+	/// </summary>
+	public partial class WorkingPage : UserControl
+	{
+		public WorkingPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
